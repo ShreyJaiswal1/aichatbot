bf28e0780cfdfdc94207526adfd132092c3cee06
Shrey Jaiswal <5aprilshrey@gmail.com>
2024-10-14 19:43:20 +0530

    Honey AI
    updated to ask username on join

--- src/public/script.js
+++ src/public/script.js
@@ -1,4 +1,37 @@
 const apiUrl = 'http://localhost:3000/api/chat';
+const socket = io({
+  reconnection: true,             // whether to reconnect automatically
+  reconnectionAttempts: Infinity, // number of reconnection attempts before giving up
+  reconnectionDelay: 1000,        // how long to initially wait before attempting a new reconnection
+  reconnectionDelayMax: 5000,
+});
+let username = null
+let joined = false
+    // autofocus on input username
+   document.getElementById("name").focus();
+
+    // username entered
+    document.getElementById('name').addEventListener("keypress", async (event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        goToChatRoom();
+      }
+    });
+function goToChatRoom() {
+  const inputValue = document.getElementById("name");
+
+  if (inputValue.value.length != 0) {
+      username = inputValue.value
+      inputValue.blur
+      inputValue.value = "";
+      document.getElementById("landing-page").remove();
+
+      if (!joined) {
+          joined = true
+          socket.emit('user-joined', username)
+      }
+  }
+}
 document.getElementById('chatInput').addEventListener('input', async function() {
     this.style.height = 'auto';
     this.style.height = (this.scrollHeight) + 'px';
@@ -17,7 +50,7 @@ document.getElementById('sendButton').addEventListener('click', async function()
         const response = await fetch(apiUrl, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ message: message })
+          body: JSON.stringify({ message: message, username: username }),
       });
     const data = await response.json();
     const chatMessage = document.createElement('div');
@@ -27,6 +60,7 @@ document.getElementById('sendButton').addEventListener('click', async function()
     scrollToBottom();
     }
 });
+
 document.getElementById('chatInput').addEventListener("keypress", async (event) => {
     if (event.key === 'Enter') {
       event.preventDefault();
@@ -37,4 +71,13 @@ document.getElementById('chatInput').addEventListener("keypress", async (event) 
   async function scrollToBottom() {
     const content = document.getElementById('chatMessages');
     content.scrollTop = content.scrollHeight;
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
 }
--- src/server.js
+++ src/server.js
@@ -7,19 +7,33 @@ const apiToken = process.env.API_TOKEN;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'))
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 // Handle API requests
+app.get("/", async (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
 app.post('/api/chat', async (req, res) => {
-    const userMsg = req.body.message;
-    const botResponse = await getAIResponse(userMsg);
+    const userMsg = req.body.message
+    const userName = req.body.username;
+    const botResponse = await getAIResponse(userMsg, userName);
 
     res.json({ reply: botResponse });
 });
 
-async function getAIResponse(userMessage) {
+io.on('connection', async (client) => {
+  client.on('user-joined', username =>  {
+    console.log(`${username} joined the chat`);
+    io.emit('user-joined', username);
+    client.broadcast.emit('user-joined', username);
+  });
+})
+async function getAIResponse(userMessage, userName) {
   const response = await fetch("https://api.groq.com/openai/v1/chat/completions", {
       method: "POST",
       headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "application/json", 
           "Authorization": `Bearer ${apiToken}`
       },
       body: JSON.stringify({
@@ -36,17 +50,17 @@ Please keep in mind:
 - Keep replies short and simple with humour
 - You should be kind and friendly, and little bit flirty as well
 
-For example, if someone asks about your favorite movie, you might say something like: "Mujhe toh 'Kabir Singh' bahut pasand hai, uski toh vibe hi alag hai!" Feel free to use such informal examples to guide your conversation style. user message: ${userMessage}` },
+For example, if someone asks about your favorite movie, you might say something like: "Mujhe toh 'Kabir Singh' bahut pasand hai, uski toh vibe hi alag hai!" Feel free to use such informal examples to guide your conversation style. user message: ${userMessage} username: ${userName}` },
           ]
       })
   });
   
   const data = await response.json();
-  console.log(data);
+  //console.log(data);
   return data.choices[0].message.content;
 }
 
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`Server started http://localhost:${port}/`);
 });
